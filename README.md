87173c482c278478234b581854cd90aaaad5c541
Alex Chaffee <alex@stinky.com>
2018-07-28 22:10:36 -0400

    move instructions to README

---
+++ README.md
@@ -0,0 +1,55 @@
+#  Today I Learned
+
+  a small example of NodeJS using MongoDB
+
+## Installation
+
+###  To install MongoDB:
+
+  * on MacOS with Homebrew, run:
+      brew update
+      brew install mongodb 
+      brew services start mongodb
+    
+    to fix the error "Permission denied @ dir_s_mkdir - /usr/local/Frameworks", run:
+      sudo mkdir /usr/local/Frameworks
+      sudo chown $(whoami):admin /usr/local/Frameworks
+
+    to watch the log, run
+      tail -f /usr/local/var/log/mongodb/mongo.log
+
+    see https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/#install-with-homebrew
+
+
+  * all others: 
+    see https://docs.mongodb.com/guides/server/install/
+
+
+### To install TIL:
+
+Clone this repo, then run
+
+    cd til
+    npm install
+    npm link
+
+  (on MacOS with Homebrew, you may also need to do
+
+    brew unlink node && brew link node
+  
+  )
+
+## Usage
+
+From the command line, run
+
+    til help
+
+To add a lesson:
+
+    til add 'eels are fish, not snakes'
+
+To list all lessons:
+
+    til list
+
--- til.js
+++ til.js
@@ -1,47 +1,18 @@
 #!/usr/bin/env node 
-/*
-  Today I Learned
-  a small example of NodeJS using MongoDB
-
-  To install MondoDB:
-
-  * on MacOS with Homebrew, run:
-      brew update
-      brew install mongodb 
-      brew services start mongodb
-    
-    to fix the error "Permission denied @ dir_s_mkdir - /usr/local/Frameworks", run:
-      sudo mkdir /usr/local/Frameworks
-      sudo chown $(whoami):admin /usr/local/Frameworks
-
-    to watch the log, run
-      tail -f /usr/local/var/log/mongodb/mongo.log
-
-    see https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/#install-with-homebrew
-
-
-  * all others: 
-    see https://docs.mongodb.com/guides/server/install/
-
-
-  To install locally:
-    npm link
-  (with homebrew, you may also need to do
-    brew unlink node && brew link node
-  )
-
-  NodeJS Mongo Driver docs and tutorial:
-    https://mongodb.github.io/node-mongodb-native/
-    http://mongodb.github.io/node-mongodb-native/3.1/tutorials/crud/
-*/
 'use strict';
+
+
+/*
+NodeJS Mongo Driver docs and tutorial:
+  https://mongodb.github.io/node-mongodb-native/
+  http://mongodb.github.io/node-mongodb-native/3.1/tutorials/crud/
+*/
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const moment = require('moment');
 
-
-// Connection URL
+// Connection URL format:
 // const url = 'mongodb://$[username]:$[password]@$[hostlist]/$[database]?authSource=$[authSource]';
 
 // see https://devcenter.heroku.com/articles/mongolab
